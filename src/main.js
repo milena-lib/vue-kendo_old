b9d11e73a485cb6ee2d6345dad71ec5358e8f9cf
milena-lib <57179624+milena-lib@users.noreply.github.com>
2021-05-07 16:57:17 +0300

    create project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@progress/kendo-theme-default/dist/all.css'
+import { Grid } from '@progress/kendo-vue-grid'
+Vue.component('Grid', Grid);
+
+new Vue({
+    el: '#app' //id of the container
+})
 
 createApp(App).mount('#app')
